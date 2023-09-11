d819e6aac5acde3c2984383fd220c539e4a6f383
hdyntrama <hadiyantrama.id@gmail.com>
2023-09-11 17:46:43 +0700

    first commit

---
+++ learnoop.js
@@ -0,0 +1,26 @@
+class Kendaraan{
+    constructor(merek,cc){
+        this.merek = merek
+        this.cc = cc
+    }
+    penjualan(){
+        console.log("penjualan dengan cc " +this.cc+" merupakan penjualan terbanyak")
+    }
+}
+
+class Motor extends Kendaraan{
+    constructor(merek, cc, warna, panjang){
+        super(merek,cc)
+        this.warna = warna
+        this.panjang = panjang
+    }
+   
+}
+
+const honda = new Motor("Vario",150,"hitam",175)
+/*console.log(honda.merek)
+console.log(honda.cc)
+console.log(honda.warna)
+console.log(honda.panjang)
+honda.penjualan()*/
+console.log(honda)
