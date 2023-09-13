7e693b930909197ba74e7c5b4b8c23d2a11ae2de
hdyntrama <hadiyantrama.id@gmail.com>
2023-09-13 13:31:09 +0700

    Add studycase

---
+++ studycase.js
@@ -0,0 +1,11 @@
+//The case is from 2 cases in HW Week 2
+
+//Case BBM
+
+//create class Kendaraan
+class Kendaraan {
+    constructor(jenisKendaraan, kapasitasMesin) {
+        this.jenisKendaraan = jenisKendaraan;
+        this.kapasitasMesin = kapasitasMesin;
+    }
+}
