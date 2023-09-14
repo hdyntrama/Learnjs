ef88dafc08571c13b861fbe4911e616b4d61635b
hdyntrama <hadiyantrama.id@gmail.com>
2023-09-14 16:42:54 +0700

    learn callback

--- async.js
+++ async.js
@@ -1,6 +1,20 @@
 //Set timeout logic
+/*
 console.log("Step 1")
 setTimeout(function(){
     console.log("Step 3")
 },5000)
 console.log("Step 2")
+*/
+
+const notify = () =>{
+    console.log("sudah selesai")
+}
+
+const startPlayGame = (nama, callback)=>{
+    console.log("start game "+nama+" sekarang!")
+    callback()
+}
+
+let nama = "valorant"
+startPlayGame(nama,notify)
